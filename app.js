792c23e61fe6afbd8db4adae35d3954295fba362
zoink506 <benhubble03@gmail.com>
2024-04-29 16:41:38 +1000

    Set up library and book objects, create addBookToLibrary function

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("test");
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(book, library) {
+  library.push(book);
+}
+
+console.log(myLibrary);
+newBook = new Book("Maze Runner", "David", 300, true);
+addBookToLibrary(newBook, myLibrary);
+console.log(myLibrary);
