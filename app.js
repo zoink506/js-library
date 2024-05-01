c8aa07d0241613d9b1207e09603c316ccc6a2ffb
zoink506 <benhubble03@gmail.com>
2024-05-01 13:11:40 +1000

    Create function that displays library in DOM

--- app.js
+++ app.js
@@ -11,7 +11,21 @@ function addBookToLibrary(book, library) {
   library.push(book);
 }
 
+function displayLibrary(library) {
+  const libraryContainer = document.getElementById("library");
+
+  for(let i = 0; i < library.length; i++) {
+    const bookElement = document.createElement("div");
+    bookElement.innerText = library[i].title;
+    libraryContainer.append(bookElement);
+  }
+}
+
 console.log(myLibrary);
 newBook = new Book("Maze Runner", "David", 300, true);
 addBookToLibrary(newBook, myLibrary);
+addBookToLibrary(new Book("Harry Potter", "J.K Rowlings", 250, false), myLibrary);
+addBookToLibrary(new Book("1984", "George Orwell", "473", true), myLibrary);
 console.log(myLibrary);
+
+displayLibrary(myLibrary);
